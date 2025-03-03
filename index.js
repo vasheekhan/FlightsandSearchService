eb136651fb58f4032cbbed4956902972b9a853c5
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2025-03-03 17:39:11 +0530

    Added body parser with body parser.json and bodyparser.urlencoded extended true

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express=require("express");
-const {PORT}=require("./src/config/serverConfig.js")
+const express = require("express");
+const { PORT } = require("./src/config/serverConfig.js");
+const bodyParser = require("body-parser");
 
-const settingUpServer=async()=>{
-    const app=express();
-    app.listen(PORT,()=>{
-        console.log(`Server is listening on the port ${PORT}` );
-       
-    })
-}
+const settingUpServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.listen(PORT, () => {
+    console.log(`Server is listening on the port ${PORT}`);
+  });
+};
 settingUpServer();
