fd147ce3d9c3e1a33dc22978494f33b0d67d68cd
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2025-03-07 14:56:36 +0530

    create a city service

---
+++ src/services/city-service.js
@@ -0,0 +1,42 @@
+const {CityRepository}=require("../repository/index")
+class CityService{
+    constructor(){
+this.cityRepository=new CityRepository();
+    }
+    async createCity(data){
+        try {
+const city=await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in city repo");
+            throw {error}   
+        }
+    }
+    async deleteCity(cityId){
+        try {
+           const response=this.cityRepository.deleteCity(cityId) 
+           return response;
+        } catch (error) {
+            console.log("Something went wrong in city repo");
+            throw {error}  
+        }
+    }
+    async updateCity(cityId,data){
+        try {
+            const city=await this.cityRepository.updateCity(cityId,data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in city repo");
+            throw {error}  
+        }  
+    }
+    async getCity(cityId){
+        try {
+           const city=await this.cityRepository.getCity(cityId) 
+           return city;
+        } catch (error) {
+            console.log("Something went wrong in city repo");
+            throw {error}  
+        } 
+    }
+}
